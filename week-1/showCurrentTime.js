1cdea38ec96c6cfd7c84020e9668dc69d160e1d0
Serafettin Bayram <sbayram2016@gmail.com>
2019-10-09 01:17:39 +0200

    exercise4 modified time.html added

--- week-1/showCurrentTime.js
+++ week-1/showCurrentTime.js
@@ -4,5 +4,8 @@ let currentTime = setInterval(getTheTime, 1000);
 
 function getTheTime() {
   let today = new Date();
+  // document.body.textContent =
+  //   today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  //     OR
   document.body.innerHTML = today.toLocaleTimeString();
 }

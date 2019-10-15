3d2168332900ace566cddcea4da1ea8b5edca168
Serafettin Bayram <sbayram2016@gmail.com>
2019-10-15 15:31:57 +0200

    exercise-2 doe4

---
+++ week-2/exercise-2-what'sYourMondayWorth.js
@@ -0,0 +1,34 @@
+'use strict';
+// When you're a developer at a big company your Monday could look something like this:
+
+const mondayTasks = [
+  {
+    name: 'Daily standup',
+    duration: 30, // specified in minutes
+  },
+  {
+    name: 'Feature discussion',
+    duration: 120,
+  },
+  {
+    name: 'Development time',
+    duration: 240,
+  },
+  {
+    name: 'Talk to different members from the product team',
+    duration: 60,
+  },
+];
+
+// Let's assume your hourly rate is €25. How much would you earn on that day?
+// Write a program that finds out what your hourly rate on a Monday would be
+// Use the map array function to take out the duration time for each task.
+// Multiply each duration by a per-hour rate for billing and sum it all up.
+// Output a formatted Euro amount, rounded to Euro cents, e.g: €11.34.
+// Make sure the program can be used on any array of objects that contain a duration property with a number value
+
+function amountOfEarn(tasks) {
+  return tasks.map(task => (task.duration * 25) / 60).reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(amountOfEarn(mondayTasks));
